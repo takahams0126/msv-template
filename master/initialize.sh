da45810cc9bce6ff37c7ef6053475b0e02b2068d
masakazu.takahashi.private <t.masa.kazu1979@gmail.com>
2020-03-08 13:38:33 +0900

    ビルド修正.

--- master/initialize.sh
+++ master/initialize.sh
@@ -10,12 +10,12 @@ mkdir -p ../db-core/docker/initdb.d
 mkdir -p ../db-core/docker/log
 
 # vscodeワークスペース設定を初期化
-sh ./gradlew --stacktrace initializeWorkspace
+sh ./gradlew --stacktrace initProject eclipse
 
 # vscodeでJava開発するプロジェクトはeclipseタスクを実行
 # vscodeでクラスパスなどを認識させるため
-sh ./gradlew --stacktrace :db-core:eclipse
-sh ./gradlew --stacktrace :db-custom:eclipse
-sh ./gradlew --stacktrace :msv-core:eclipse
-sh ./gradlew --stacktrace :msv-service1:eclipse
+#sh ./gradlew --stacktrace :db-core:eclipse
+#sh ./gradlew --stacktrace :db-custom:eclipse
+#sh ./gradlew --stacktrace :msv-core:eclipse
+#sh ./gradlew --stacktrace :msv-service1:eclipse
 
